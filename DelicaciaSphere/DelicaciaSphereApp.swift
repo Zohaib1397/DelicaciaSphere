a4fb2648f689861df51424624150f236af7b952b
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2024-08-13 22:41:44 +0500

    Initial Commit

---
+++ DelicaciaSphere/DelicaciaSphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelicaciaSphereApp.swift
+//  DelicaciaSphere
+//
+//  Created by Zohaib Ahmed on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DelicaciaSphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DelicaciaSphereTests/DelicaciaSphereTests.swift
@@ -0,0 +1,17 @@
+//
+//  DelicaciaSphereTests.swift
+//  DelicaciaSphereTests
+//
+//  Created by Zohaib Ahmed on 8/13/24.
+//
+
+import Testing
+@testable import DelicaciaSphere
+
+struct DelicaciaSphereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
